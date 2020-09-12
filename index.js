b4e1830daedb9ea5bc026ccab6da3dc8741ab7ee
Krylov Sergey <kssergeyru@gmail.com>
2020-09-12 22:35:19 +0300

    Post request to Mine Block

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {Blockchain} = require('./blockchain/blockchain');
 const {PORT} = require('./config');
+const bodyParser = require('body-parser');
 
 // Объект приложения
 const app = express();
@@ -8,10 +9,22 @@ const app = express();
 // Blockchain
 const blockchain = new Blockchain();
 
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+    console.log(`listening at localhost: ${PORT}`);
+});
+
+// Получение всех блоков
 app.get('/api/blocks', (req, res) => {
     res.json(blockchain.chain)
 });
 
-app.listen(PORT, () => {
-    console.log(`listening at localhost: ${PORT}`);
-})
+// Создание нового блока
+app.post('/api/mine', (req, res) => {
+    const {data} = req.body;
+
+    blockchain.addBlock({data})
+
+    res.redirect('/api/blocks');
+});
